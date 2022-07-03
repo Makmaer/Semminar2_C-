db26f131cf16fb41c0d3d4a8e0298a248f4965e0
Makmaer <instrojgrad@gmail.com>
2022-07-03 21:58:01 +0500

    Zadacha 2

---
+++ Zadacha2_SluchainoeChisloIzOtrezka/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int FindBiggerDigit()
+{
+    int rand = new Random().Next(10, 100);
+    Console.WriteLine("Sluchaynoe chislo = " + rand);
+    int dec = rand / 10;
+    int ed = rand % 10;
+    if(dec > ed) return dec;
+    else return ed;
+}
+
+int maxNum = FindBiggerDigit();
+Console.WriteLine("Bolshaya cifra =" + maxNum);
