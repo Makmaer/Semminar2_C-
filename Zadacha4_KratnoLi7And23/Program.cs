7bfc7d435bf16183782fcaf088fba316b7247871
Makmaer <instrojgrad@gmail.com>
2022-07-03 23:00:29 +0500

    Zadacha 4. проверка числа на кратность семи и двадцати трем

---
+++ Zadacha4_KratnoLi7And23/Program.cs
@@ -0,0 +1,16 @@
+﻿void FindBigger(int n1)
+{
+    if (n1 % 7 == 0 && n1 % 23 == 0)
+{
+    Console.WriteLine("Chislo kratno 7 and 23 ");
+}
+else {
+   
+    Console.WriteLine("ne kratno" );
+
+}
+    }
+int namb=161;
+
+
+FindBigger(namb);
