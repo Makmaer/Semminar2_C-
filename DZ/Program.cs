a5e6f680275551725667cab042b9eb16cc1c5a91
Makmaer <instrojgrad@gmail.com>
2022-07-03 23:20:42 +0500

    Zadacha 10. выводим среднюю цифру трехзначного числа

---
+++ DZ/Program.cs
@@ -0,0 +1,10 @@
+﻿int FirstDigit()
+{
+    int rand = new Random().Next(100, 1000);
+    Console.WriteLine("Sluchaynoe chislo = " + rand);
+    int des = rand % 100 / 10;
+       return des;
+}
+
+int seredina = FirstDigit();
+Console.WriteLine("Srednyy cifra =" + seredina);
