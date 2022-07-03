0061cb3c066154fcfc91e9f0fd0b23eb687fb677
Makmaer <instrojgrad@gmail.com>
2022-07-03 22:24:33 +0500

    Zadacha 3. выяснить кртно ли второе первому

---
+++ Zadacha3_KratnoLiVtoroePervomu/Program.cs
@@ -0,0 +1,16 @@
+﻿void FindBigger(int n1, int n2)
+{
+    if (n2 % n1 == 0)
+{
+    Console.WriteLine("Wtoroe kratno pervomu ");
+}
+else {
+    int ost = n2 % n1;
+    Console.WriteLine("Ostatok ot delenia " + " " + ost );
+
+}
+    }
+int namb1=3;
+int namb2=63;
+
+FindBigger(namb1, namb2);
