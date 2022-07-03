52fe17ed06c17c21bd4166da20b3314c7a2cb5d8
Makmaer <instrojgrad@gmail.com>
2022-07-03 21:22:07 +0500

    Zadacha 1

--- Zadacha1_ssummaDvuhChisel/Program.cs
+++ Zadacha1_ssummaDvuhChisel/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*int sum (int a1, int a2)   // Метод с возвращением
+{
+int summa = a1 + a2;
+return summa;
+}
+int result = sum (2,3);
+Console.WriteLine (result);*/
+
+
+void sum (int a1, int a2)  // метод без возвращения
+{
+int summa = a1 + a2;
+Console.WriteLine (summa);
+}
+sum (2,3);
